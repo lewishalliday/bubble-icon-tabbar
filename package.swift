7b9ac4c9460af4f41450e5d3b85c63ff6aa3143d
Lewis Halliday <lewishalliday@gmail.com>
2024-01-29 21:32:05 -0500

    SPM Tweak

--- package.swift
+++ package.swift
@@ -1,22 +1,23 @@
-// swift-tools-version:5.1
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "BubbleTabBar",
-    platforms: [.iOS(.v10)],
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v11)
+    ],
     products: [
         .library(
             name: "BubbleTabBar",
-            targets: ["BubbleTabBar"]
-        ),
+            targets: ["BubbleTabBar"]),
     ],
+    dependencies: [],
     targets: [
         .target(
             name: "BubbleTabBar",
-            path: "BubbleTabBar"
-        ),
-    ],
-    swiftLanguageVersions: [.v5]
+            dependencies: [])
+    ]
 )
